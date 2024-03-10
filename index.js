1d32c6fc246edaa65553ebee934ff58fd2c3e69d
someCatInTheWorld <162684669+someCatInTheWorld@users.noreply.github.com>
2024-03-10 19:21:41 +1100

    Get rid of more Bark mentions.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log("Exit Now! This part is used for devolopers.");
 
 // navbar
 content = '<div class="navbar"><a href="home.html"><img src="src/images/Logo.svg" alt="bark" width="25" height="25" ></a><a href="editor_new/">create</a><a href="explore.html">explore</a>'
-content += '<a href="ideas.html">ideas</a><a href="about.html">about</a><a href="https://barkprogram.boards.net/">discuss</a><a href="login.html" class="right">sign in</a><a href="signup.html" class="right">join bark</a>'
+content += '<a href="ideas.html">ideas</a><a href="about.html">about</a><a href="login.html" class="right">sign in</a><a href="signup.html" class="right">join bark</a>'
 content += '<a class="dark-mode-button" id="darkModeToggle"><dmbico alt="Dark Mode"></dmbico></a><input class="searchBar" placeholder="Search for Projects"></input></div>'
     document.getElementById('navbar').innerHTML = content;
 
